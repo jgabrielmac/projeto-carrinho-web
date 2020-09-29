b1db47bb936a3abae3e2fc2b4a8ed7b71c2a2a86
jgabrielmac <azevedojgabriel@gmail.com>
2020-09-29 17:10:54 -0300

    eslint changes

--- src/Componentes/Pages/CartPage/index.jsx
+++ src/Componentes/Pages/CartPage/index.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import { useDispatch, useSelector } from 'react-redux';
-import { Creators as CarrinhoCreator } from '../../../Store/Ducks/carrinhoReducer';
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Creators as CarrinhoCreator } from "../../../Store/Ducks/carrinhoReducer";
 import { Link } from "react-router-dom";
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from "@material-ui/core/styles";
 import {
   Grid,
   Card,
@@ -13,85 +13,113 @@ import {
   Button,
   Typography,
   Container,
-} from '@material-ui/core';
-import { RemoveMessageSnackbars } from '../../Snackbars';
-import SendButton from '../../SendButton';
+} from "@material-ui/core";
+import { RemoveMessageSnackbars } from "../../Snackbars";
+import SendButton from "../../SendButton";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   media: {
     height: 100,
     width: 100,
-    padding: '2em',
+    padding: "2em",
     margin: 30,
-    display: 'block',
-    marginLeft: 'auto',
-    marginRight: 'auto',
+    display: "block",
+    marginLeft: "auto",
+    marginRight: "auto",
   },
   title: {
-    height: '80px',
-    color: 'black'
+    height: "80px",
+    color: "black",
   },
 }));
 
 const CartPage = () => {
-
-  const dispatch = useDispatch()
-  const { carrinho } = useSelector(state => state.carrinho)
+  const dispatch = useDispatch();
+  const { carrinho } = useSelector((state) => state.carrinho);
   const [localState, setLocalState] = React.useState({
     openRemoveMessage: false,
-  })
-  const { openRemoveMessage } = localState
+  });
+  const { openRemoveMessage } = localState;
 
   React.useEffect(() => {
-    dispatch(CarrinhoCreator.cartList())
-  }, [dispatch])
+    dispatch(CarrinhoCreator.cartList());
+  }, [dispatch]);
 
   const Remover = (id) => {
-    dispatch(CarrinhoCreator.removeCart(id))
+    dispatch(CarrinhoCreator.removeCart(id));
     setLocalState({
-      openRemoveMessage: true
-    })
-  }
+      openRemoveMessage: true,
+    });
+  };
+
+  // const sameProducts = carrinho.filter((prod, anotherProd) => {
+  //   const prods = [];
+
+  //   if (prod.id === anotherProd.id) {
+  //     return prods.push(prod, anotherProd);
+  //   } else {
+  //     return carrinho;
+  //   }
+  // });
+  // console.log(sameProducts);
 
   const handleCloseRemoveMessage = () => {
     setLocalState({
-        openRemoveMessage: false
-    })
-}
-  const classes = useStyles()
+      openRemoveMessage: false,
+    });
+  };
+  const classes = useStyles();
 
   return (
     <Container maxWidth="xl" fixed>
       <Grid container spacing={5}>
         {carrinho.map((item) => (
-          <Grid item xs={12} sm={6} md={4} lg={3} xl={2} key={Math.random()}>
+          <Grid item xs={12} sm={6} md={4} lg={3} xl={2} key={item.id}>
             <Card className={classes.card}>
               <CardActionArea>
-                <Link to={`/product/${item.id}`} style={{ textDecoration: 'none' }}>
+                <Link
+                  to={`/product/${item.id}`}
+                  style={{ textDecoration: "none" }}
+                >
                   <CardMedia className={classes.media} image={item.picture} />
                   <CardContent>
                     <Typography gutterBottom className={classes.title}>
                       {item.title.substring(0, 70)}...
                     </Typography>
-                    <Typography variant="body2" color="textSecondary" component="p">
+                    <Typography
+                      variant="body2"
+                      color="textSecondary"
+                      component="p"
+                    >
                       {`R$ ${item.price}`}
                     </Typography>
                   </CardContent>
                 </Link>
               </CardActionArea>
               <CardActions>
-                <Button size="small" color="primary" onClick={() => Remover(item.id)}>Remover</Button>
+                <Button
+                  size="small"
+                  color="primary"
+                  onClick={() => Remover(item.id)}
+                >
+                  Remover
+                </Button>
               </CardActions>
             </Card>
           </Grid>
         ))}
-        {openRemoveMessage ? <RemoveMessageSnackbars openRemoveMessage handleCloseRemoveMessage={handleCloseRemoveMessage} /> : null}
-        <Link to="/finalizar-pedido/" style={{ textDecoration: 'none' }}>
+        {openRemoveMessage ? (
+          <RemoveMessageSnackbars
+            openRemoveMessage
+            handleCloseRemoveMessage={handleCloseRemoveMessage}
+          />
+        ) : null}
+        <Link to="/finalizar-pedido/" style={{ textDecoration: "none" }}>
           <SendButton buttonMessage={"Finalizar Pedido"} />
         </Link>
       </Grid>
     </Container>
-  )
-}
+  );
+};
 
 export default CartPage;
