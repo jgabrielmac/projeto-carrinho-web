8c51aea5ee432ba505325714738e6d86bf704bf5
jgabrielmac <azevedojgabriel@gmail.com>
2020-10-16 11:54:36 -0300

    formatada arquivo de redux

--- src/Store/Ducks/carrinhoReducer.js
+++ src/Store/Ducks/carrinhoReducer.js
@@ -1,55 +1,53 @@
-
 export const Types = {
-    ADD_CART: 'ADD_CART',
-    REMOVE_CART: 'REMOVE_CART',
-    CART_LIST: 'CART_LIST'
-}
+  ADD_CART: "ADD_CART",
+  REMOVE_CART: "REMOVE_CART",
+  CART_LIST: "CART_LIST",
+};
 
 const initialState = {
-    carrinho: []
-}
+  carrinho: [],
+};
 
 const removeItem = (id, carrinho) => {
-    var newCarrinho = carrinho;
-    const indexItem = newCarrinho.findIndex(prod => prod.id === id);
+  const indexItem = carrinho.findIndex((prod) => prod.id === id);
 
-    if (indexItem >= 0) {
-        newCarrinho.splice(indexItem, 1);
-    }
-    return newCarrinho
+  if (indexItem >= 0) {
+    carrinho.splice(indexItem, 1);
+  }
+  return carrinho;
 };
 
 export default (state = initialState, action) => {
-    switch (action.type) {
-        case Types.ADD_CART:
-            return {
-                ...state,
-                carrinho: [...state.carrinho, action.payload]
-            }
-        case Types.REMOVE_CART:
-            return {
-                ...state,
-                carrinho: removeItem(action.id, state.carrinho)
-            }
-        case Types.CART_LIST:
-            return {
-                ...state,
-            }
-        default:
-            return state
-    }
-}
+  switch (action.type) {
+    case Types.ADD_CART:
+      return {
+        ...state,
+        carrinho: [...state.carrinho, action.payload],
+      };
+    case Types.REMOVE_CART:
+      return {
+        ...state,
+        carrinho: removeItem(action.id, state.carrinho),
+      };
+    case Types.CART_LIST:
+      return {
+        ...state,
+      };
+    default:
+      return state;
+  }
+};
 
 export const Creators = {
-    addCart: (product) => ({
-        type: Types.ADD_CART,
-        payload: product
-    }),
-    removeCart: ( id ) => ({
-        type: Types.REMOVE_CART,
-        id: id
-    }),
-    cartList: () => ({
-        type: Types.CART_LIST,
-    })
-}
+  addCart: (product) => ({
+    type: Types.ADD_CART,
+    payload: product,
+  }),
+  removeCart: (id) => ({
+    type: Types.REMOVE_CART,
+    id: id,
+  }),
+  cartList: () => ({
+    type: Types.CART_LIST,
+  }),
+};
